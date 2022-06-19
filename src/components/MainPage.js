e9821e9951ad9190b0c0053b3bf467604328199e
김다영 <greenseed33@naver.com>
2022-06-19 21:39:15 +0900

    상품리스트 api 연동

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,6 @@
 import "./MainPage.css";
 import axios from "axios";
 
-git config --global user.name "김다영"
-git config --global user.email "greenseed33@naver.com"
-
-
 const MainPage = () => {
   let [products, setProducts] = React.useState([]);
   const url =`https://546c0a94-ea04-42e0-8e83-badb01b043d7.mock.pstmn.io/products`;
@@ -12,34 +8,57 @@ const MainPage = () => {
 	React.useEffect(()=>{
     axios
       .get(url)
-      .then(result) => {
-        const products=result.data.products;
+      .then((result) => {
+        products = result.data.products;
         setProducts(products)
-      };
-      .catch(error) => {
-        console.log(`통신실패:, ${error}`);
+      })
+      .catch((error) => {
+        console.log(`통신실패: ${error}`);
       });
-    },[])
-	return (
-		<>
-			<div id="header">
-       <div id="header-area">
-           <img src="icons/logo.png" alt="" />
-       </div>
-      </div>
-      <div id="body">
-          <div id="banner">
-              <img src="banners/banner1.png" alt="" />
+    }, []);
+
+  {
+    products.map((product, idx)=>{
+      return()
+    });
+  }
+      return (
+        <>
+          <div id="header">
+          <div id="header-area">
+              <img src="icons/logo.png" alt="" />
           </div>
-          <h1>Products</h1>
-          <div id="product-list">
           </div>
-      </div>
-      <div id="footer">
-          <a href="#">회사소개</a><a href="#">이용약관</a><a href="#">통신판매업:123-1234</a><a href="#">사업자등록번호:456-56-78951</a><a href=""></a>
-      </div> 
-    </>
-  )
+          <div id="body">
+              <div id="banner">
+                  <img src="banners/banner1.png" alt="" />
+              </div>
+              <h1>Products</h1>
+              <div id="product-list">
+                {products.map((product, idx)=>{
+                  return(
+                    <div className="product-card" key={idx}>
+                      <div>
+                        <img className="product-img" src={product.imgURL} alt="" />
+                      </div>
+                      <div className="product-contents">
+                        <div className="product-name">{product.name}</div>
+                        <div className="product-price">{product.price}</div>
+                        <div className="product-seller">
+                          <img src="images/icons/avatar.png" alt="" className="" />
+                          <span>{product.seller}</span>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+          </div>
+          <div id="footer">
+              <a href="#">회사소개</a><a href="#">이용약관</a><a href="#">통신판매업:123-1234</a><a href="#">사업자등록번호:456-56-78951</a><a href=""></a>
+          </div> 
+        </>
+      );
 };
 
-export default component;
+export default MainPage;
